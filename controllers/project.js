ca5f4f334a9079536a0a418631e2088d636d5b44
albertomfe <albertomfe@gmail.com>
2020-03-28 20:53:06 -0600

    se agrego metodo guardar portafolio

--- controllers/project.js
+++ controllers/project.js
@@ -1,5 +1,8 @@
 'use strict'
 
+var Project=require('../models/project');//Importar el Modelo
+
+
 var controller={
 
   home:function(req,res){
@@ -9,11 +12,35 @@ var controller={
   },
 
   test:function(req,res){
-    res.status(200).send({
+    return res.status(200).send({
         message:"soy el metodo test del controlador"
     });
-  }
+  },
 
+  saveProject:function(req,res){
+
+    var project=new Project();//crear un Objeto de proyecto
+
+    var params=req.body;
+    project.name=params.name;
+    project.description=params.description;
+    project.category=params.category;
+    project.langs=params.langs;
+    project.year=params.year;
+    project.image=null;
+
+    /*guardar el objeto en la base de datos con funcion callback*/
+    project.save((err,projectStore)=>{
+      if(err) return res.status(500).send({message:"error al Guardar"});
+      if(!projectStore)return res.status(404).send({message:"no se pudo guardar el proyecto"});
+      return res.status(200).send({project:projectStore});
+    });
+
+    /*return  res.status(200).send({
+      project:project,
+      message:"proyecto Guardado"
+    });*/
+  }
 
 
 };
--- models/project.js
+++ models/project.js
@@ -2,18 +2,21 @@
 
 var mongoose = require('mongoose');//cargar el modulo mongoose para la conexion
 var Schema=mongoose.Schema;
+//mongoose.connect('mongodb://localhost:27017/portafolio', {  useUnifiedTopology: true, useNewUrlParser: true,useCreateIndex: true })
 
 //crear el Modelo de la Tabla
 var projectSchema=Schema({
   name:String,
-  Description:String,
+  description:String,
   category:String,
   langs:[String],
-  year:Number
+  year:Number,
+  image:String
 });
 
 /*
 primer parametro es el nombre de la collecion donde guardar lo hace minusculas y lo pluraliza
 segundo paraetro es el modelo a usar
 */
-module.exports | mongoose.model('Project',projectSchema);
+module.exports = mongoose.model('Project',projectSchema);
+//module.exports | mongoose.model('Project',projectSchema);
